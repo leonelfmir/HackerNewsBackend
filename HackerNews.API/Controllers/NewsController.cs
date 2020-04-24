5e0ad03633e8a1e6b339444eeb6f1f0f00400b30
leonel fernandez <leone>
2020-04-24 16:52:08 -0400

    changed controller to NewsController

---
+++ HackerNews.API/Controllers/NewsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNews.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        public void SearchTitle([FromBody] string value)
+        {
+        }
+
+
+        
+    }
+}
