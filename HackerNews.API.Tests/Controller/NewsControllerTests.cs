ca64d6a062827bbaf27cd9bf7ec1fc1a8faf3359
leonel fernandez <leone>
2020-04-25 10:12:33 -0400

    All tests working

--- HackerNews.API.Tests/Controller/NewsControllerTests.cs
+++ HackerNews.API.Tests/Controller/NewsControllerTests.cs
@@ -84,12 +84,73 @@ namespace HackerNews.API.Tests.Controller
             
         }
 
+        [Theory, AutoData]
+        public async Task Search_ShouldCallNewsServiceOnce(List<New> newsFixture, NewsSearchResource searchValue)
+        {
+            // Arrange
+            var mockedNewsService = createNewsServiceSearch(newsFixture);
+            var controller = new NewsController(_mapper, mockedNewsService.Object);
+
+            // Act
+            var result = await controller.SearchTitle(searchValue);
+
+            // Asset
+            mockedNewsService.Verify(svc => svc.SearchAsync(searchValue.Search), Times.Once());
+        }
+
+        [Theory, AutoData]
+        public async Task Search_ShouldReturnBadRequestWithMessageIfErroOnService(NewsSearchResource searchValue)
+        {
+            // Arrange
+            const string errorMessage = "Error";
+            var mockNewsService = new Mock<INewsService>();
+            var responseFixture = new GenericResponse<IEnumerable<New>>(errorMessage);
+            mockNewsService.Setup(svc => svc.SearchAsync(searchValue.Search)).ReturnsAsync(responseFixture);
+            var controller = new NewsController(_mapper, mockNewsService.Object);
+
+            // Act
+            var result = await controller.SearchTitle(searchValue);
+
+            // Asset
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            badRequestResult.Value.Should().Be(errorMessage);
+        }
+
+        [Theory, AutoData]
+        public async Task Search_FieldsShouldBeTheSame(List<New> newsFixture, NewsSearchResource searchValue)
+        {
+            // Arrange
+            var mockedNewsService = createNewsServiceSearch(newsFixture);
+            var controller = new NewsController(_mapper, mockedNewsService.Object);
+
+            // Act
+            var result = await controller.SearchTitle(searchValue);
+
+            // Asset
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnNews = Assert.IsAssignableFrom<IEnumerable<NewResource>>(okResult.Value);
+            returnNews.Should().Equal(newsFixture, (rsc, fix) =>
+            {
+                return rsc.Author == fix.By && rsc.Title == fix.Title && rsc.Url == fix.Url;
+            });
+            
+        }
+
         private Mock<INewsService> createNewsService(List<New> newsFixture)
         {
             var responseFixture = new GenericResponse<IEnumerable<New>>(newsFixture);
 
             var mockNewsService = new Mock<INewsService>();
             mockNewsService.Setup(svc => svc.ListAsync()).ReturnsAsync(responseFixture);
+            return mockNewsService;
+        }
+        
+        private Mock<INewsService> createNewsServiceSearch(List<New> newsFixture)
+        {
+            var responseFixture = new GenericResponse<IEnumerable<New>>(newsFixture);
+
+            var mockNewsService = new Mock<INewsService>();
+            mockNewsService.Setup(svc => svc.SearchAsync(It.IsAny<string>())).ReturnsAsync(responseFixture);
             return mockNewsService;
         }
     }
