7ef723fee96cfffae488b46859db1027e87e3e71
leonel fernandez <leone>
2020-04-25 13:36:48 -0400

    Update cache working

--- HackerNews.Persistence/Repositories/NewsRepository.cs
+++ HackerNews.Persistence/Repositories/NewsRepository.cs
@@ -15,7 +15,8 @@ namespace HackerNews.Persistence.Repositories
     public class NewsRepository : INewsRepository
     {
         private readonly IMemoryCache _memoryCache;
-
+        
+        private const string cacheKey = "news";
         private readonly string _maxItemUrl;
         private readonly string _getItemUrl;
         private readonly string _newIdsUrl;
@@ -39,9 +40,54 @@ namespace HackerNews.Persistence.Repositories
             return news.Where(n => n != null).Where(n => n.Title.Contains(value, StringComparison.InvariantCultureIgnoreCase));
         }
 
+        /// <summary>
+        /// Updates cache to get new stories
+        /// </summary>
+        /// <returns></returns>
+        private async Task UpdateCache()
+        {
+            var oldCache = await IsCacheOld();
+
+            if(!oldCache.isOld)
+            {
+                return;
+            }
+
+            await AddNewItemsToCache(oldCache.maxItem, oldCache.lastItem);
+        }
+
+        private async Task AddNewItemsToCache(int maxItem, int lastItem)
+        {
+            var numberOfNewItems = maxItem - lastItem;
+            var newsIds = Enumerable.Range(maxItem + 1, numberOfNewItems);
+            var getNews = newsIds.Select(GetNewById);
+
+            var newNews = await Task.WhenAll(getNews);
+            var currentNews = await GetNews();
+
+            var newList = currentNews.Concat(newNews);
+
+            _memoryCache.Set(cacheKey, newList);
+        }
+
+        private async Task<(bool isOld, int lastItem, int maxItem)> IsCacheOld()
+        {
+            var maxItemTask = GetMaxItem();
+            var newsTask = GetNews();
+
+            await Task.WhenAll(maxItemTask, newsTask);
+
+            var news = await newsTask;
+            var maxItem = await maxItemTask;
+
+            var lastItem = news.Max(n => n.Id);
+
+            return (lastItem > maxItem, lastItem, maxItem);
+        }
+
         private async Task<IEnumerable<New>> GetNews()
         {
-            var res = await _memoryCache.GetOrCreateAsync("news", entry =>
+            var res = await _memoryCache.GetOrCreateAsync(cacheKey, entry =>
             {
                 return GetNewsFromApi();
             });
